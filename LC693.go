b6b8387eda2680d3e0e2cb35673453fa1e0e208c
heweshub <wendahe1996@163.com>
2022-03-29 00:01:04 +0800

    today, i will update this repositories 20220328 LC693 done!

---
+++ LC693.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func convertTen2bin(n int) string {
+	s := ""
+	for ; n > 0; n /= 2 {
+		s = strconv.Itoa(n%2) + s
+	}
+	return s
+}
+func hasAlternatingBits(n int) (ans bool) {
+	s := convertTen2bin(n)
+	for i := 0; i < len(s)-1; i++ {
+		if s[i] == s[i+1] {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	flag := hasAlternatingBits(11)
+	fmt.Println(flag)
+}
