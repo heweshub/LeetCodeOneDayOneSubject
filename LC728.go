297efff314e6ee3105aca5afb7e50f2b7346828b
heweshub <wendahe1996@163.com>
2022-03-31 19:46:04 +0800

    728

---
+++ LC728.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func selfDividingNumbers(left int, right int) []int {
+	if left > right {
+		return []int{}
+	}
+	var cnt []int
+	for i := left; i <= right; i++ {
+		if check(i) {
+			cnt = append(cnt, i)
+		}
+	}
+	return cnt
+}
+
+func check(num int) bool {
+	n := num
+	for n > 0 {
+		yuShu := n % 10
+		if yuShu == 0 || num%yuShu != 0 {
+			return false
+		}
+		n /= 10
+	}
+	return true
+}
+
+func main() {
+	fmt.Println(selfDividingNumbers(47, 85))
+}
