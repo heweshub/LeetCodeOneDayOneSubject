6321faad3f9f6b4e9dbe48d96a3da8c8116df4d5
heweshub <wendahe1996@163.com>
2022-04-02 17:38:45 +0800

    solved 420 这题是又臭又长啊

---
+++ LC420.go
@@ -0,0 +1,92 @@
+package main
+
+import "unicode"
+
+func strongPasswordChecker(password string) int {
+
+	hasUpper, hasLower, hasDigit := 0, 0, 0
+	for _, ch := range password {
+		if unicode.IsLower(ch) {
+			hasLower = 1
+		} else if unicode.IsUpper(ch) {
+			hasUpper = 1
+		} else if unicode.IsDigit(ch) {
+			hasDigit = 1
+		}
+	}
+
+	catagories := hasUpper + hasLower + hasDigit
+
+	switch n := len(password); {
+	case n < 6:
+		return max(6-n, 3-catagories)
+	case n <= 20:
+		replace, cnt, cur := 0, 0, '#'
+		for _, ch := range password {
+			if ch == cur {
+				cnt++
+			} else {
+				replace += cnt / 3
+				cnt = 1
+				cur = ch
+			}
+		}
+		replace += cnt / 3
+		return max(replace, 3-catagories)
+	default:
+		replace, remove := 0, n-20
+		rm2, cnt, cur := 0, 0, '#'
+		for _, ch := range password {
+			if ch == cur {
+				cnt++
+				continue
+			}
+			if remove > 0 && cnt >= 3 {
+				if cnt%3 == 0 {
+					remove--
+					replace--
+				} else if cnt%3 == 1 {
+					rm2++
+				}
+			}
+			replace += cnt / 3
+			cnt = 1
+			cur = ch
+		}
+		if remove > 0 && cnt >= 3 {
+			if cnt%3 == 0 {
+				remove--
+				replace--
+			} else if cnt%3 == 1 {
+				rm2++
+			}
+		}
+
+		replace += cnt / 3
+		// 使用 k % 3 = 1 的组的数量，由剩余的替换次数、组数和剩余的删除次数共同决定
+		use2 := min(min(replace, rm2), remove/2)
+		replace -= use2
+		remove -= use2 * 2
+
+		// 由于每有一次替换次数就一定有 3 个连续相同的字符（k / 3 决定），因此这里可以直接计算出使用 k % 3 = 2 的组的数量
+		use3 := min(replace, remove/3)
+		replace -= use3
+		remove -= use3 * 3
+		return (n - 20) + max(replace, 3-catagories)
+
+	}
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func min(x, y int) int {
+	if x > y {
+		return y
+	}
+	return x
+}
