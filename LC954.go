a8d7e46277f870c96a5cf7cd7c12a00dae5d2807
heweshub <wendahe1996@163.com>
2022-04-02 10:53:24 +0800

    solved 954

---
+++ LC954.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func canReorderDoubled(arr []int) bool {
+	n := len(arr)
+	cnt := make(map[int]int, n)
+	for _, x := range arr {
+		cnt[x]++
+	}
+	if cnt[0]%2 != 0 {
+		return false
+	}
+	vals := make([]int, 0, len(cnt))
+	for x := range cnt {
+		vals = append(vals, x)
+	}
+	sort.Slice(vals, func(i, j int) bool { return abs(vals[i]) < abs(vals[j]) })
+	for _, x := range vals {
+		if cnt[2*x] < cnt[x] {
+			return false
+		}
+		cnt[2*x] -= cnt[x]
+	}
+	return true
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func main() {
+	fmt.Println(canReorderDoubled([]int{2, 4, 0, 0, 8, 1}))
+}
