a901aa0e7396bc4b06c7fc0ffc283df55c18138a
heweshub <wendahe1996@163.com>
2022-04-12 16:09:46 +0800

    806

---
+++ LC806.go
@@ -0,0 +1,20 @@
+package main
+
+func numberOfLines(widths []int, s string) (ans []int) {
+	n := len(s)
+	cnt := 0
+	lines := 1
+	for i := 0; i < n; i++ {
+		cnt += widths[s[i]-'a']
+		if cnt > 100 {
+			cnt = widths[s[i]-'a']
+			lines++
+		}
+	}
+	//fmt.Println(lines, cnt)
+	return []int{lines, cnt}
+}
+
+func main() {
+	numberOfLines([]int{4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10}, "bbbcccdddaaa")
+}
