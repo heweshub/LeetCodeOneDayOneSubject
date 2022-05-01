fb10df973212ed7a2f615f3d8798348fd351871f
heweshub <wendahe1996@163.com>
2022-05-01 11:26:50 +0800

    291zhousai

--- aaa.py
+++ aaa.py
@@ -20,5 +20,25 @@ def intersection(nums):
     return ans.sort()
 
 
+def appealSum(s):
+    cnt = 0
+    n = len(s)
+    for i in range(1, n + 1):
+        for j in range(n):
+            if j + i > n:
+                break
+            else:
+                cnt += dupli(s[j:i + j])
+    return cnt
+
+
+def dupli(ss):
+    liS = [ss[i] for i in range(len(ss))]
+    return len(set(liS))
+
+
 if __name__ == '__main__':
-    intersection([[44, 21, 48]])
+    # intersection([[44, 21, 48]])
+    # print(dupli("aabd"))
+    print(appealSum("abbca"))
+    print(appealSum("code"))
