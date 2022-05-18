cfce7a217e0a762dee3b7ec08d2da074968b2bea
heweshub <wendahe1996@163.com>
2022-05-18 15:04:40 +0800

    668

---
+++ LC668.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func findKthNumber(m, n, k int) int {
+	return sort.Search(m*n, func(x int) bool {
+		count := x / n * n
+		for i := x/n + 1; i <= m; i++ {
+			count += x / i
+		}
+		return count >= k
+	})
+}
+
+func main() {
+	fmt.Println(findKthNumber(2, 3, 6))
+}
