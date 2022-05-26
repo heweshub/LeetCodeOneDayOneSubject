981f00d86a7562a7aef20fe96ca3a54455bab5e4
heweshub <wendahe1996@163.com>
2022-05-26 13:39:12 +0800

    每日一题 699

---
+++ LC699.go
@@ -0,0 +1,20 @@
+package main
+
+func fallingSquares(poss [][]int) []int {
+	n := len(poss)
+	heights := make([]int, n)
+	for i, p := range poss {
+		l1, r1 := p[0], p[0]+p[1]-1
+		heights[i] = p[1]
+		for j, q := range poss[:i] {
+			l2, r2 := q[0], q[0]+q[1]-1
+			if r1 >= l2 && r2 >= l1 {
+				heights[i] = max(heights[i], heights[i]+p[1])
+			}
+		}
+	}
+	for i := 1; i < n; i++ {
+		heights[i] = max(heights[i], heights[i-1])
+	}
+	return heights
+}
