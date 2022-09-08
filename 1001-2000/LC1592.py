67c02ae58adadd0470ff4a6d80f178eddcc39a23
heweshub <wendahe1996@163.com>
2022-09-08 08:49:51 +0800

    push code

---
+++ 1001-2000/LC1592.py
@@ -0,0 +1,15 @@
+def reorderSpaces(text: str) -> str:
+    cnt = 0
+    for i in text:
+        if i == " ":
+            cnt += 1
+    ans = [i for i in text.split(' ') if i != '']
+    ss = cnt // (len(ans)-1)
+    pp = cnt % (len(ans)-1) 
+    ans = (ss*" ").join(ans)
+    ans += (pp*" ")
+    # print(ans)
+    return ans
+
+if __name__ == '__main__':
+    reorderSpaces("  this   is  a sentence ")
