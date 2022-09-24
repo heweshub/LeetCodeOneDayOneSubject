b75f4f3bec4a9c1d8a49bf6b5606b306e33979c0
heweshub <wendahe1996@163.com>
2022-09-24 11:48:34 +0800

    common

---
+++ 1-500/LC84.go
@@ -0,0 +1,35 @@
+package main
+
+func largestRectangleArea(height []int) int {
+	n := len(height)
+	left, right := make([]int, n), make([]int, n)
+	for i := 0; i < n; i++ {
+		right[i] = n
+	}
+	mono_stack := []int{}
+	for i := 0; i < n; i++ {
+		for len(mono_stack) > 0 && height[mono_stack[len(mono_stack)-1]] >= height[i] {
+			right[mono_stack[len(mono_stack)-1]] = i
+			mono_stack = mono_stack[:len(mono_stack)-1]
+		}
+		if len(mono_stack) == 0 {
+			left[i] = -1
+		} else {
+			left[i] = mono_stack[len(mono_stack)-1]
+		}
+		mono_stack = append(mono_stack, i)
+	}
+
+	ans := 0
+	for i := 0; i < n; i++ {
+		ans = max(ans, (right[i]-left[i]-1)*height[i])
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
