9c0db4a897b6955ec50d38a926bf08f4a71d5717
heweshub <wendahe1996@163.com>
2022-12-07 13:46:18 +0800

    12月继续刷题

--- 1-500/LC146.go
+++ 1-500/LC146.go
@@ -1,82 +1,118 @@
 package main
 
+import "container/list"
+
+//type LRUCache struct {
+//	size       int
+//	capacity   int
+//	cache      map[int]*DLinkedNode
+//	head, tail *DLinkedNode
+//}
+//
+//type DLinkedNode struct {
+//	key, value int
+//	prev, next *DLinkedNode
+//}
+//
+//func initDLinkedNode(key, value int) *DLinkedNode {
+//	return &DLinkedNode{
+//		key:   key,
+//		value: value,
+//	}
+//}
+//
+//func Constructor146(capacity int) LRUCache {
+//	l := LRUCache{
+//		cache:    map[int]*DLinkedNode{},
+//		head:     initDLinkedNode(0, 0),
+//		tail:     initDLinkedNode(0, 0),
+//		capacity: capacity,
+//	}
+//	l.head.next = l.tail
+//	l.tail.prev = l.head
+//	return l
+//}
+//
+//func (this *LRUCache) Get(key int) int {
+//	if _, ok := this.cache[key]; !ok {
+//		return -1
+//	}
+//	node := this.cache[key]
+//	this.moveToHead(node)
+//	return node.value
+//}
+//
+//func (this *LRUCache) Put(key, value int) {
+//	if _, ok := this.cache[key]; !ok {
+//		node := initDLinkedNode(key, value)
+//		this.cache[key] = node
+//		this.addToHead(node)
+//		this.size++
+//		if this.size > this.capacity {
+//			removed := this.removeTail()
+//			delete(this.cache, removed.key)
+//			this.size--
+//		}
+//	} else {
+//		node := this.cache[key]
+//		node.value = value
+//		this.moveToHead(node)
+//	}
+//}
+//
+//func (this *LRUCache) addToHead(node *DLinkedNode) {
+//	node.prev = this.head
+//	node.next = this.head.next
+//	this.head.next.prev = node
+//	this.head.next = node
+//}
+//
+//func (this *LRUCache) removeNode(node *DLinkedNode) {
+//	node.prev.next = node.next
+//	node.next.prev = node.prev
+//}
+//
+//func (this *LRUCache) moveToHead(node *DLinkedNode) {
+//	this.removeNode(node)
+//	this.addToHead(node)
+//}
+//
+//func (this *LRUCache) removeTail() *DLinkedNode {
+//	node := this.tail.prev
+//	this.removeNode(node)
+//	return node
+//}
+
+// 直接使用list内置的数据结构来实现
+type entry struct{ key, value int }
+
 type LRUCache struct {
-	size       int
-	capacity   int
-	cache      map[int]*DLinkedNode
-	head, tail *DLinkedNode
+	cap   int
+	cache map[int]*list.Element
+	lst   *list.List
 }
 
-type DLinkedNode struct {
-	key, value int
-	prev, next *DLinkedNode
-}
+//func Constructor(capacity int) LRUCache {
+//	return LRUCache{capacity, map[int]*list.Element{}, list.New()}
+//}
 
-func initDLinkedNode(key, value int) *DLinkedNode {
-	return &DLinkedNode{
-		key:   key,
-		value: value,
-	}
-}
-
-func Constructor146(capacity int) LRUCache {
-	l := LRUCache{
-		cache:    map[int]*DLinkedNode{},
-		head:     initDLinkedNode(0, 0),
-		tail:     initDLinkedNode(0, 0),
-		capacity: capacity,
-	}
-	l.head.next = l.tail
-	l.tail.prev = l.head
-	return l
-}
-
-func (this *LRUCache) Get(key int) int {
-	if _, ok := this.cache[key]; !ok {
+func (c *LRUCache) Get(key int) int {
+	e := c.cache[key]
+	if e == nil {
 		return -1
 	}
-	node := this.cache[key]
-	this.moveToHead(node)
-	return node.value
+	c.lst.MoveToFront(e)
+	return e.Value.(entry).value
 }
 
-func (this *LRUCache) Put(key, value int) {
-	if _, ok := this.cache[key]; !ok {
-		node := initDLinkedNode(key, value)
-		this.cache[key] = node
-		this.addToHead(node)
-		this.size++
-		if this.size > this.capacity {
-			removed := this.removeTail()
-			delete(this.cache, removed.key)
-			this.size--
-		}
-	} else {
-		node := this.cache[key]
-		node.value = value
-		this.moveToHead(node)
+func (c *LRUCache) Put(key, value int) {
+	if e := c.cache[key]; e != nil {
+		e.Value = entry{key, value}
+		c.lst.MoveToFront(e)
+		return
 	}
-}
-
-func (this *LRUCache) addToHead(node *DLinkedNode) {
-	node.prev = this.head
-	node.next = this.head.next
-	this.head.next.prev = node
-	this.head.next = node
-}
-
-func (this *LRUCache) removeNode(node *DLinkedNode) {
-	node.prev.next = node.next
-	node.next.prev = node.prev
-}
-
-func (this *LRUCache) moveToHead(node *DLinkedNode) {
-	this.removeNode(node)
-	this.addToHead(node)
-}
-
-func (this *LRUCache) removeTail() *DLinkedNode {
-	node := this.tail.prev
-	this.removeNode(node)
-	return node
+	c.cache[key] = c.lst.PushFront(entry{key, value})
+	if len(c.cache) > c.cap {
+		delete(c.cache, c.lst.Remove(c.lst.Back()).(entry).key)
+	}
 }
